b30502978fc06e02bcc97f1b5e20d4a58aed65ba
aşkın gedik <askn@bil.omu.edu.tr>
2012-03-30 21:09:30 +0300

    ilkle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osce::Application.config.secret_token = 'f61a64d5885e3222a60e5a254a646c4e238ec39a5dd553c97a2eda932dc7c88e91f7866ba7ab95667855a862bcb6d9186df73eb632c424c77fb3e2ac68a94d09'
